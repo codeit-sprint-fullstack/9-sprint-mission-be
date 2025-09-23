34300d9349727d082bda6ff0107c08728ddb6a1f
rklpoi5678 <rklpoi5678@naver.com>
2025-09-23 16:08:24 +0900

    Feat: logger, reqTimer 미들웨어 추가

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
 import express from "express";
 import { connectDB } from "./db/index.js";
-import { config } from "./config/config.js";
+import { config, isDevelopment } from "./config/config.js";
 import { router } from "./routes/index.js";
 import { cors } from "./middlewares/cors.js";
+import { logger } from "./middlewares/logger.js";
+import { reqTimer } from "./middlewares/reqTimer.js";
 
 const app = express();
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+if (isDevelopment) {
+  app.use(logger);
+  app.use(reqTimer);
+}
 
 app.use(cors);
 
