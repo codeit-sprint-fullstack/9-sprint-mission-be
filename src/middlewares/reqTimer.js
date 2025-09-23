081e02101f55748ed93482435175c44f11c1bf7b
rklpoi5678 <rklpoi5678@naver.com>
2025-09-23 18:13:39 +0900

    Fix: 오타수정

--- src/middlewares/reqTimer.js
+++ src/middlewares/reqTimer.js
@@ -1,7 +1,7 @@
 export const reqTimer = (req,res,next) => {
   const startTime = Date.now();
   
-  res.on('finsh', () => {
+  res.on('finish', () => {
     const duration = Date.now() = startTime;
     console.log(`reqTime: ${duration}ms`);
   });
