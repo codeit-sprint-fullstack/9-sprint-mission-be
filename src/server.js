9d3f5a15edf25d06fb55ef40c583560f5baca79a
rklpoi5678 <rklpoi5678@naver.com>
2025-09-23 14:02:48 +0900

    Chore: 패키지설치 및 설정

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log("Server running on http://localhost" + PORT);
+});
