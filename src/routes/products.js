ef410c176cea83ab13ba5452dd3027a4c359577c
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-09-26 00:22:53 +0900

    feat: Implement additional features for the API that provides full product list

--- src/routes/products.js
+++ src/routes/products.js
@@ -8,7 +8,34 @@ const mokItems = [
     price: 900000,
     tags: ['스마트폰', '애플'],
     createdAt: 1,
-    updatedAt: 1,
+    updatedAt: 0,
+  },
+  {
+    id: 1,
+    name: '테스트1',
+    description: '테스트1',
+    price: 1,
+    tags: ['테스트'],
+    createdAt: 2,
+    updatedAt: 0,
+  },
+  {
+    id: 2,
+    name: '테스트2',
+    description: '테스트2',
+    price: 2,
+    tags: ['테스트'],
+    createdAt: 3,
+    updatedAt: 0,
+  },
+  {
+    id: 3,
+    name: '테스트3',
+    description: '테스트3',
+    price: 3,
+    tags: ['테스트'],
+    createdAt: 4,
+    updatedAt: 0,
   },
 ];
 
@@ -17,17 +44,47 @@ let newId = 1;
 export const productsRouter = express.Router();
 
 productsRouter.get('/', (req, res) => {
-  res.json({ success: true, data: mokItems, count: mokItems.length });
+  const page = parseInt(req.query.page, 10) || 1;
+  const pageSize = parseInt(req.query.pageSize, 10) || 10;
+  const keyword = req.query.keyword;
+  const offset = (page - 1) * pageSize;
+
+  const responsItems = keyword
+    ? mokItems
+        .filter((item) => {
+          item.name.includes(keyword) || item.description.includes(keyword);
+        })
+        .slice(offset, offset + pageSize)
+        .map((item) => {
+          (item.id, item.name, item.price, item.createdAt);
+        })
+    : mokItems.slice(offset, offset + pageSize).map((item) => {
+        (item.id, item.name, item.price, item.createdAt);
+      });
+
+  res.json({
+    success: true,
+    data: responsItems,
+    page,
+    pageSize,
+    offset,
+    total: mokItems.length,
+  });
 });
 
 productsRouter.get('/:id', (req, res) => {
   const { id } = parseInt(req.params.id);
-  res.json({ productsId: id });
-});
+  const patchIndex = mokItems.findIndex((u) => u.id === id);
+  if (patchIndex === -1) {
+    return res.status(404).json({
+      success: false,
+      message: '상품을 찾을 수 없습니다',
+    });
+  }
 
-productsRouter.get('/', (req, res) => {
-  const { saerch } = req.query;
-  res.json({ saerch: saerch });
+  const responsItem = mokItems.find((item) => item.id === id);
+
+  res.json({ success: true, data: responsItem });
 });
 
 productsRouter.post('/', (req, res) => {
