836f0a99dc8fcaac9da35b349efb32996a6937e1
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-09-26 03:28:06 +0900

    feat: set middlewares

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import { router } from './routes/index.js';
+import { logger } from './middlewares/logger.js';
+import { requestTimer } from './middlewares/requestTimer.js';
+import { config, isDevelopment } from './config/config.js';
 
 const app = express();
-const PORT = 3000;
 
-// JSON 파싱 미들웨어
 app.use(express.json());
 
-// 모든 라우트 등록
+app.use(express.urlencoded({ extended: true }));
+
+if (isDevelopment) {
+  app.use(logger);
+  app.use(requestTimer);
+}
+
 app.use('/', router);
 
-// 서버 시작
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${config.PORT}`);
+  console.log(`📦 Environment: ${config.ENVIRONMENT}`);
 });
