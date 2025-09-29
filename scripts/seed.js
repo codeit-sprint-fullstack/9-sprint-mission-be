d0191b93b55a735fc1590c54c385bd289a167917
rklpoi5678 <rklpoi5678@naver.com>
2025-09-29 22:20:51 +0900

    Refactor: 마이그레이션- 프리즈마 데이터 스키마 설정
    - 시딩 데이터변경
    - config/config.js DB_URI를 postgresql로수정
    - .env.exapme예시 postgresql로 수정

---
+++ scripts/seed.js
@@ -0,0 +1,80 @@
+import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker/locale/ko";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const NUM_USERS_TO_CREATE = 5;
+  console.log("시딩 시작");
+
+  const userPromise = Array.from({ length: NUM_USERS_TO_CREATE }).map(() =>
+    prisma.user.create({
+      data: {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        userProfile: {
+          create: {
+            photoUrl: faker.image.avatar(),
+            bio: faker.person.bio(),
+          },
+        },
+      },
+    }),
+  );
+
+  const users = await Promise.all(userPromise);
+
+  const allPosts = [];
+  for (const user of users) {
+    const postCount = faker.number.int({ min: 1, max: 5 });
+    const postPromises = Array.from({ length: NUM_USERS_TO_CREATE }).map(() => {
+      const randomUser =
+        users[faker.number.int({ min: 0, max: users.length - 1 })];
+      return prisma.article.create({
+        data: {
+          title: faker.lorem.sentence({ min: 3, max: 14 }),
+          content: faker.lorem.paragraph({ min: 2, max: 7 }),
+          images: [
+            faker.image.urlPicsumPhotos({ width: 100, height: 100 }),
+            faker.image.urlPicsumPhotos({ width: 100, height: 100 }),
+            faker.image.urlPicsumPhotos({ width: 100, height: 100 }),
+          ],
+          authorId: randomUser.id,
+        },
+      });
+    });
+
+    const userPosts = await Promise.all(postPromises);
+    allPosts.push(...userPosts);
+  }
+  console.log(`${allPosts.length}개의 게시물이 생성되었습니다.`);
+
+  for (const post of allPosts) {
+    const commentCount = faker.number.int({ min: 1, max: 10 });
+    const commentPromises = Array.from({ length: commentCount }).map(() => {
+      const randomUser =
+        users[faker.number.int({ min: 0, max: users.length - 1 })];
+      return prisma.comment.create({
+        data: {
+          context: faker.lorem.sentences({ min: 1, max: 3 }),
+          authorId: randomUser.id,
+          articleId: post.id,
+        },
+      });
+    });
+
+    await Promise.all(commentPromises);
+  }
+
+  console.log(`${users.length}명의 유저가 생성되었습니다.`);
+  console.log("데이터 시딩 완료");
+}
+main()
+  .catch((e) => {
+    console.error("Seeding Error", e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
