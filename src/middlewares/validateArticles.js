458711e73823a2fd8a42fd04cb21a4a66850431c
Lim Gyungmin <ancora.gyungmin@gmail.com>
2025-10-01 11:10:35 +0900

    fix: fix wrong fild name context to content

--- src/middlewares/validateArticles.js
+++ src/middlewares/validateArticles.js
@@ -1,13 +1,13 @@
 import { BadRequestException } from '../err/badRequestException.js';
 
 export const validateArticles = (req, res, next) => {
-  const { title, context } = req.body;
+  const { title, content } = req.body;
 
   if (!title) {
     throw new BadRequestException('제목은 존재해야합니다.');
   }
 
-  if (!context || context.trim().length < 10) {
+  if (!content || content.trim().length < 10) {
     throw new BadRequestException(
       '내용은 존재해야하며, 10자 이상이여야 합니다.',
     );
