cb5b557c331d7c9a6644440f57c2223e6d84309a
rklpoi5678 <rklpoi5678@naver.com>
2025-12-23 11:50:20 +0900

    Refactor: ts마이그레이션 (prisma 설정)

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,22 +1,26 @@
-// prisma client 설정
-import { PrismaClient } from "../generated/client.ts";
+import { PrismaClient, type Prisma } from "../generated/client.js";
 import { exit } from "node:process";
 import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-const getPrismaLogLevel = () => {
+const getPrismaLogLevel = (): Prisma.LogLevel[] => {
   if (process.env.NODE_ENV === "production") {
     return ["warn", "error"];
   }
   return ["query", "info", "warn", "error"];
 };
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+const connectionString = process.env.DATABASE_URL as string;
+
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({
   adapter,
   log: getPrismaLogLevel(),
 });
 
-export const connectDB = async () => {
+export const connectDB = async (): Promise<void> => {
   try {
     await prisma.$connect();
     console.log("프리스마가 성공적으로 연결되었습니다.");
@@ -26,7 +30,7 @@ export const connectDB = async () => {
   }
 };
 
-export const disconnectDB = async () => {
+export const disconnectDB = async (): Promise<void> => {
   await prisma.$disconnect();
   console.log("Prisma Disconnected");
 };
