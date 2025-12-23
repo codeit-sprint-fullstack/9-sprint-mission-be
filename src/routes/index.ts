ee0e56795173a8935a507316a6a4fb8243c7cf4a
rklpoi5678 <rklpoi5678@naver.com>
2025-12-23 13:47:08 +0900

    Refactor: ts마이그레이션 (index)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
+import type { Request, Response, Router } from "express";
 import { itemRouter } from "./v1/items.route.js";
 import { articleRouter } from "./v1/articles.route.js";
 import { authRouter } from "./v1/auth.route.js";
 import { userRouter } from "./v1/users.route.js";
 
-export const router = express.Router();
+export const router: Router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", (req: Request, res: Response) => {
   res.json({
     message: "Hello Panda!",
     timestamp: new Date().toISOString(),
