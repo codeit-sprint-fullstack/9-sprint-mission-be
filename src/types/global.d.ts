14e051cb3f1af5b246f296a8d91531a8a36c58cd
rklpoi5678 <rklpoi5678@naver.com>
2025-12-23 14:39:37 +0900

    Feat: req.auth확장

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,5 +1,12 @@
 declare namespace Express {
   export interface Request {
     valid?: boolean;
+    auth?: {
+      id: string;
+      email: string;
+      iat: number;
+      exp: number;
+      // JWT Payload
+    };
   }
 }
