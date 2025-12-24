cfa605d9fa9eb0d30bfc782a33e7fea7006779fd
rklpoi5678 <rklpoi5678@naver.com>
2025-12-24 14:40:50 +0900

    Fix: 이미 레포에서 select로 패스워드,토큰을 제외

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -10,18 +10,15 @@ import { User } from "../types/user";
 export class UserService {
   constructor(private readonly userRepository:UserRepository) {}
 
-  /**
-   * 미들웨어에서 추출된 유저아이디 바탕으로 유저 정보를 반환 
-   */
-  async getAuthenticatedUser(userId: string):Promise<Omit<User,"encryptedPassword"  | "refreshToken">> {
+  /** 미들웨어에서 추출된 유저아이디 바탕으로 유저 정보를 반환 */
+  async getAuthenticatedUser(userId: string):Promise<Pick<User,"id"  | "email" | "nickname">> {
     const user = await this.userRepository.findUserById(userId);
 
     if (!user) {
       throw new NotFoundException(NOT_FOUND_USER);
     }
 
-    const { password, refreshToken, ...userProfile} = user;
-    return userProfile;
+    return user;
   }
 }
 
