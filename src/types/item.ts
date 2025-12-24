30fbce0ed5da92be77bec50772e98e07acf57823
rklpoi5678 <rklpoi5678@naver.com>
2025-12-24 13:39:48 +0900

    Refactor: ts마이그레이션(item서비스)

--- src/types/item.ts
+++ src/types/item.ts
@@ -1,21 +1,26 @@
-export type Item =  {
-    id: string,
-    authorId: string,
-    name: string,
-    description: string,
-    price: string,
-    images: string[],
-    createdAt: Date,
-    updatedAt: Date,
-    itemComment: ItemComment
-}
+import type { Prisma } from "../generated/client";
+
+export type Item = {
+  id: string;
+  authorId: string;
+  name: string;
+  description: string;
+  price: string;
+  images: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  itemComment: ItemComment;
+};
 
 export type ItemComment = {
-    id: string;
-    userId:  string;
-    itemId: string;
-    createdAt: Date;
-    updatedAt: Date;
-}
+  id: string;
+  userId: string;
+  itemId: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
-export  type OrderByType = "recent" | "oldest"
+export type OrderByType = "recent" | "oldest";
+export type ItemUpdateDto = Omit<Prisma.ItemUpdateInput, "tags"> & {
+    tags?: string[]
+};
