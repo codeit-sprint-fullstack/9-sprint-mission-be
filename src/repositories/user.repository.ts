87388b96991ed838431d6eef1eeb2992654af962
rklpoi5678 <rklpoi5678@naver.com>
2025-12-24 09:51:45 +0900

    Feat: 유저 타입추가

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,18 +1,19 @@
 import { prisma } from "../db/prisma";
-import type { PrismaClient, Prisma } from "../generated/client";
-import { User } from "../types/user";
+import type { PrismaClient } from "../generated/client";
+import { UserSelectProfile } from "../types/user";
 
 export class UserRepository {
   constructor(private readonly prisma: PrismaClient) {}
 
   /** 유저 아이디로 공개된 사용자정보를 찾습니다. */
-  async findUserById(userId: string):Promise<Pick<User, "id" | "email" | "nickname"> | null> {
+  async findUserById(userId: string):Promise<UserSelectProfile | null> {
     return this.prisma.user.findUnique({
       where: { id: userId },
       select: {
         id: true,
         email: true,
         nickname: true,
+        // 추가적인 필드가 없으므로 리턴타입은 Pick이 적합
       },
     });
   }
