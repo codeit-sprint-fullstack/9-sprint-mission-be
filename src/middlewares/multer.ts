90064352d56cb3005a415e232f0fdba1455be017
rklpoi5678 <rklpoi5678@naver.com>
2026-01-02 14:32:14 +0900

    Refactor: multer s3로 변환

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,33 +1,41 @@
 /** @see https://inpa.tistory.com/entry/EXPRESS-%F0%9F%93%9A-multer-%EB%AF%B8%EB%93%A4%EC%9B%A8%EC%96%B4 */
 import type { Request } from "express";
 import multer, { FileFilterCallback } from "multer";
+import multerS3 from "multer-s3";
 import path from "path";
-import fs from "fs";
 import { BadRequestException } from "../common/exceptions/error";
 import { v4 as uuidv4 } from "uuid";
+import { S3Client } from "@aws-sdk/client-s3";
 
 /** uploads 디렉토리 설정 및 초기화 */
-const uploadDir = "uploads";
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
+// const uploadDir = "uploads";
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir);
+// }
+
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
 
 /**
  *  storage setting: dev env -> LocalDisk | Production -> multerS3
  */
 // const storage = isProduction ? multerS3(S3) : multer.diskStorage(기존로컬)
-const storage = multer.diskStorage({
-  // 저장위치
-  destination: (_req: Request, _file: Express.Multer.File, done): void => {
-    done(null, uploadDir);
-  },
-  // 파일 이름 생성 규칙: UUID를 사용하여 중복 방지
-  filename: (_req: Request, file: Express.Multer.File, done): void => {
+const storage = multerS3({
+  s3: s3,
+  bucket: process.env.AWS_S3_BUCKET_NAME!,
+  acl: 'public-read',                                                     // 권한: 업로드된 파일을 외부에서 읽을 수 있게 한다.
+  contentType: multerS3.AUTO_CONTENT_TYPE,    // 파일 mimetype 자동 설정 (매우 중요!)
+  key: (_req,file,done) => {
     const randomId = uuidv4();
     const ext = path.extname(file.originalname);
-    const filename = `${randomId}${ext}`;
-    done(null, filename);
-  },
+    const filename  = `uploads/${randomId}${ext}`
+    done(null, filename)
+  } 
 });
 
 /**  파일 필터링: 허용된 확장자(JPEG, JPG, PNG)만 업로드 허용 */
